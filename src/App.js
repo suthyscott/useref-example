6795d8a3576ec239762c8ed7f2632e3518f55b6a
suthyscott <sutherlandscott21@gmail.com>
2023-11-16 16:09:42 -0700

    setting up example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState, useEffect, useRef } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState("")
+    // const [renderCount, setRenderCount] = useState(1)
+    // const renderCount = useRef(1)
+
+    const inputRef = useRef()
+
+    const prevName = useRef('')
+
+    useEffect(() => {
+      // setRenderCount(prevRenderCount => prevRenderCount + 1)  
+      // renderCount.current = renderCount.current + 1
+
+      prevName.current = name
+    })
+
+    function focus(){
+      inputRef.current.focus()
+      // don't use refs to hold data, that should always be done with a state value so it's connected to the lifecycle. 
+    }
+
+    return (
+        <div className="App">
+            {/* <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} /> */}
+            <input value={name} onChange={e => setName(e.target.value)} />
+            <p>My name is {name}</p>
+            <p>My name is {name} and it used to be {prevName.current}</p>
+
+
+            {/* <p>I rendered {renderCount} times</p> */}
+            {/* <p>I rendered {renderCount.current} times</p> */}
+
+            {/* <button onClick={() => focus()}>Focus</button> */}
+        </div>
+    )
 }
 
-export default App;
+export default App
